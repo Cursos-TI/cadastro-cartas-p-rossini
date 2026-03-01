85655fd8a51a0e533a849301351cc816ecb38181
p-rossini <00-falada.milhao@icloud.com>
2026-02-28 23:21:57 -0300

    mantem arquivo original e cria arquivos para cada nivel

---
+++ nivel_novato.c
@@ -0,0 +1,38 @@
+
+// Desafio Super Trunfo - Países
+// Tema 1 - Cadastro das cartas
+
+int main() {
+  // Área para definição das variáveis
+  char estado; 
+  char codigo[3];
+  char cidade[20];
+  int populacao;
+  float area;
+  float PIB;
+  int pontos_tur_qtd;
+
+  // Área para entrada de dados
+
+  // Área para exibição dos dados da cidade
+
+return 0;
+} 
+
+
+
+/*
+int numero;
+float valor;
+double valorMaior;
+char letra;
+char nome[50];   // string
+
+%d -> inteiro (decimal)
+%i -> inteiro (equivalente a %d)
+%f -> número decimal (ponto flutuante)
+%e -> decimal em notação científica
+%c -> caractere único
+%s -> string (texto)
+\n -> pula linha 
+*/
