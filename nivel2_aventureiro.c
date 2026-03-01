e4bee67b0f483db37856cd1de188b3f3be011d3a
p-rossini <00-falada.milhao@icloud.com>
2026-02-28 23:55:31 -0300

    atualiza nomes arquivos / inicia aventureiro

---
+++ nivel2_aventureiro.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main(){
+
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+#include <stdio.h>
+
+int main(){
+
+}
+
+int numero;
+float valor;
+double valorMaior;
+char letra;
+char nome[50];   // string
+
+%d -> inteiro (decimal)
+%i -> inteiro (equivalente a %d)
+%f -> número decimal (ponto flutuante)
+%e -> decimal em notação científica
+%c -> caractere único
+%s -> string (texto)
+\n -> pula linha 
+
+*/
